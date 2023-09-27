712d1653aa824dca1c7e16aab9ad8feea9dba4fc
Lithe Jniad <99078068@mydavinci.nl>
2023-09-27 13:53:03 +0200

    push

---
+++ Toets/toets_bootcamp_opdracht.py
@@ -0,0 +1,72 @@
+# opdrach 1:
+# a. je kan de fout en je code makkelijk zien en hijn geeft aan waar precies de fout ligt. afkortingen in codes
+# b. zodat je makkelijk kan terug kijken naar je code jij of iemand anders die wilt mee kijken 
+
+# opdracht 2:
+# Maak het commentaar af.
+a = 5  # dit is een voorbeeld van het datatype: geheel getal zonder komma
+b = 10.5# dit is een voorbeeld van het datatype: kommagetal
+c = "Hallo wereld" # dit is een voorbeeld van het datatype: een tekst
+
+# opdracht 3:
+a = 5
+b = 10
+
+wissel = a
+a = b 
+b = wissel
+
+print(f"a = {a}, b = {b}") 
+
+# opdracht 4
+leeftijd = int(input("Hoe oud ben je? "))
+print(f"Dan duurt het nog ongeveer {67 - leeftijd} jaar voordat je met pensioen mag!")
+
+
+# Opdracht 5: 
+def som(a, b, c):
+    return a + b + c
+
+getal1 = 200
+getal2 = 5
+getal3 = 12
+antwoord = som(getal1, getal2, getal3)
+
+print(f"De som van {getal1} + {getal2} + {getal3} = {antwoord}")
+
+# opdrach 6
+AANTAL_GB = 20
+AANTAL_MINUTEN = 200 
+ONBEPERKT = False 
+aantal_minuten_gebeld = int(input("Hoeveel minuten heb je gebeld?"))
+aantal_GB_internet = int(input("Hoeveel GB's heb je gebruikt?"))
+
+
+if not ONBEPERKT and aantal_minuten_gebeld > AANTAL_MINUTEN or aantal_GB_internet > AANTAL_GB :
+    print("Let op: je moet bijbetalen!")
+
+    
+
+else:
+    print("Niet aan de hand gebruik je mobiel lekker verder!")
+
+
+# opdarcht 7
+for x in range(1, 251): 
+    print(x)
+
+
+# Opdracht 8:
+lijst_eten = ['appel', 'pannenkoek', 'kiwi', 'hamburger']
+
+
+print("Onze menukaart:")
+for gerecht in lijst_eten:
+    print(gerecht)
+
+
+langste_naam = max(lijst_eten, key=len)
+print(f"\nHet eten met de langste naam op het menu is: {langste_naam}")
+
+
+ 
